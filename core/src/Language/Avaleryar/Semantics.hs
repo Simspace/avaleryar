90b73eac94358caa499f9579f358f2d8a5e0786e
Alec <alec@simspace.com>
2019-08-27 10:52:46 -0400

    haddock for Semantics.hs

--- core/src/Language/Avaleryar/Semantics.hs
+++ core/src/Language/Avaleryar/Semantics.hs
@@ -8,6 +8,57 @@
 {-# LANGUAGE TypeApplications           #-}
 {-# LANGUAGE UndecidableInstances       #-}
 
+{-|
+
+Evaluation proceeds pretty much like in the Soutei paper.  Computations are performed in the
+'AvaleryarT m' monad, which is built up from the paper's backtracking monad and maintains the
+runtime state ('RT').  The latter consists of the current variable substitution, an 'Epoch' counter
+for a supply of guaranteed-fresh variables, and a database of predicates 'Db'.
+
+To 'resolve' a 'Goal' (really just a 'BodyLit'), we load its associated assertion, look its 'Pred'
+up, and execute the rule (or native predicate) to which it's bound.  Our "compiled" representation
+of rules amounts to fuctions from 'Lit's (the head of a rule) to 'AvaleryarT' computations.
+
+The database distinguishes between 'Rule's and "native predicates".  In the original implementation,
+all mode-restricted predicates were baked into the @application@ assertion.  But we expect to have a
+larger number of built-in predicates (for example, to parse JWTs, manipulate dates and times, or
+consult a SQL database), so it seemed worthwhile to deviate from the paper to allow native
+predicates to come from assertions other than @application@.  Because native predicates are likely
+to be mode restricted (one wouldn't want to backtrack through a signature-checking routine,
+attempting to enumerate new bitstrings until one just so happened to be a digest of your plaintext),
+we need some means of identifying them at load-time to mode-check them before attempting to call
+them.  To simplify all this, we require that native predicates (the only mode-restricted predicates
+in the system) reside in "native assertions", which maintain enough information in their
+'NativePred's to allow mode-checking for subsequent assertion submissions.
+
+Currently, native assertions are distinguished lexically from normal rule assertions by prefixing
+their name with a colon.  Thus, @:ldap says user-group(?user, ?group)@ refers to the native
+predicate @user-group\/2@ within the native assertion named @ldap@.  Variables may not currently
+denote native assertions, so there's no way to express something like:
+
+@
+  may(read) :-
+    application says directory-service(?ds),
+    application says user(?user),
+    :?ds says valid-user(?user).
+@
+
+With the syntax suggesting that the application might have sent @directory-service(ldap)@ along with
+its query.  The colon syntax was selected to be evocative of possibly someday in the future having
+signatures for native assertions, so we might one day write:
+
+@
+  may(read) :-
+    application says directory-service(?ds),
+    application says user(?user),
+    DS:?ds says valid-user(?user).
+@
+
+and ensure well-modedness from the signature @DS@ of all directory service assertions.
+
+-}
+
+
 module Language.Avaleryar.Semantics where
 
 import           Control.Applicative
@@ -25,14 +76,19 @@ import Control.Monad.FBackTrackT
 
 import Language.Avaleryar.Syntax
 
-
+-- | A native predicate carries not just its evaluation function, but also its signature, so it may
+-- be consulted when new assertions are submitted in order to mode-check them.
 data NativePred m = NativePred
   { nativePred :: Lit EVar -> AvaleryarT m ()
   , nativeSig  :: ModedLit
   }
 
+-- | Regular 'Rule' assertions may be named by any 'Value'.
 newtype RulesDb  m = RulesDb  { unRulesDb  :: Map Value (Map Pred (Lit EVar -> AvaleryarT m ())) }
   deriving (Semigroup, Monoid)
+
+-- | Native predicates are lexically restricted, so 'NativeDb's are keyed on 'Text' rather than
+-- 'Value'.
 newtype NativeDb m = NativeDb { unNativeDb :: Map Text (Map Pred (NativePred m)) }
   deriving (Semigroup, Monoid)
 
@@ -49,39 +105,56 @@ instance Monoid (Db m) where
   mempty = Db mempty mempty
   mappend = (<>)
 
+-- | As 'Map.lookup', but fail into 'empty' instead of 'Nothing' when the key is missing.
 alookup :: (Alternative f, Ord k) => k -> Map k a -> f a
 alookup k m = maybe empty pure $ Map.lookup k m
 
+-- | Look up a the 'Pred' in the assertion denoted by the given 'Value', and return the code to
+-- execute it.
 loadRule :: (Monad m) => Value -> Pred -> AvaleryarT m (Lit EVar -> AvaleryarT m ())
 loadRule c p = gets (unRulesDb . rulesDb . db) >>= alookup c >>= alookup p
 
+-- | As 'loadRule' for native predicates.
 loadNative :: Monad m => Text -> Pred -> AvaleryarT m (Lit EVar -> AvaleryarT m ())
 loadNative n p = gets (unNativeDb . nativeDb . db) >>= alookup n >>= alookup p >>= pure . nativePred
 
+-- | Runtime state for 'AvaleryarT' computations.
 data RT m = RT
-  { env   :: Env
-  , epoch :: Epoch
-  , db    :: Db m
+  { env   :: Env   -- ^ The accumulated substitution
+  , epoch :: Epoch -- ^ A counter for generating fresh variables
+  , db    :: Db m  -- ^ The database of compiled predicates
   }
 
+-- | A fair, backtracking, terminating, stateful monad transformer that does all the work.  This is
+-- 'StateT' over 'Stream', so state changes are undone on backtracking.  This is important.
 newtype AvaleryarT m a = AvaleryarT { unAvaleryarT :: StateT (RT m) (Stream m) a }
   deriving (Functor, Applicative, Alternative, Monad, MonadPlus, MonadFail, MonadState (RT m), MonadYield, MonadIO)
 
+-- | Run an 'AvaleryarT' computation.  The first argument is an upper limit on the number of
+-- backtracking steps the computation may take before terminating, the second is an upper limit on
+-- the number of values the computation may produce before terminating.  Both could be made optional
+-- (unlimited depth, unlimited answers), but that doesn't seem like the point of what we're trying
+-- to do here.
 runAvalaryarT :: Monad m => Int -> Int -> Db m -> AvaleryarT m a -> m [a]
 runAvalaryarT x y db = runM (Just x) (Just y)
                      . flip evalStateT (RT mempty 0 db)
                      . unAvaleryarT
 
+-- | Try to find a binding for the given variable in the current substitution.
+--
+-- NB: The resulting 'Term' may still be a variable.
 lookupEVar :: Monad m => EVar -> AvaleryarT m (Term EVar)
 lookupEVar ev = do
   RT {..} <- get
   alookup ev env
 
+-- | As 'lookupEVar', using the current value of the 'Epoch' counter in the runtime state.
 lookupVar :: Monad m => TextVar -> AvaleryarT m (Term EVar)
 lookupVar v = do
   ev <- (,) <$> gets epoch <*> pure v
   lookupEVar ev
 
+-- | Unifies two terms, updating the substitution in the state.
 unifyTerm :: (Monad m) => Term EVar -> Term EVar -> AvaleryarT m ()
 unifyTerm t t' = do
   ts  <- subst t
@@ -93,18 +166,26 @@ unifyTerm t t' = do
       (_, Var v) -> put rt {env = Map.insert v ts  env}
       _          -> empty -- ts /= ts', both are values
 
+-- | Apply the current substitution on the given 'Term'.  This function does path compression: if it
+-- finds a variable, it recurs.  This function does not fail: if there is no binding for the given
+-- variable, it will give it right back.
 subst :: Monad m => Term EVar -> AvaleryarT m (Term EVar)
 subst v@(Val _)    = pure v
 subst var@(Var ev) = gets env >>= maybe (pure var) subst . Map.lookup ev
 
 type Goal = BodyLit EVar
 
+-- | Analyze the given assertion reference and look up the given predicate to find some code to
+-- execute.
 loadResolver :: (Monad m) => ARef EVar -> Pred -> AvaleryarT m (Lit EVar -> AvaleryarT m ())
 loadResolver (ARNative n) p = loadNative n p
 loadResolver (ARTerm   t) p = do
   Val c <- subst t -- mode checking should assure that assertion references are ground by now
   loadRule c p
 
+-- | Load the appropriate assertion, and execute the predicate in the goal.  Eagerly substitutes,
+-- which I think might be inefficient, but I also think was tricky to not-do here way back when I
+-- wrote this.
 resolve :: (Monad m) => Goal -> AvaleryarT m (Lit EVar)
 resolve (assn `Says` lit@(Lit p as)) = do
   -- Val c <- subst assn
@@ -135,6 +216,7 @@ compilePred rules (Lit _ qas) = do
         traverse_ resolve body
   msum $ go <$> rules'
 
+-- | Turn a list of 'Rule's into a map from their names to code that executes them.
 compileRules :: (Monad m) => [Rule TextVar] -> Map Pred (Lit EVar -> AvaleryarT m ())
 compileRules rules = fmap compilePred $ Map.fromListWith (++) [(p, [r]) | r@(Rule (Lit p _) _) <- rules]
 
@@ -156,16 +238,24 @@ retractRuleAssertion assn = RulesDb . Map.delete (T assn) . unRulesDb
 
 ---------------------
 
-data Solely a = Solely a
-
 inMode :: Mode TextVar
 inMode = In "+"
 
 outMode :: Mode TextVar
 outMode = Out "-"
 
+-- | Typeclass machinery for easing the creation of native predicates.  The idea is to do our best
+-- to translate regular Haskell functions into predicates callable from soutei code without needing
+-- to concern ourselves with the intricacies of the evaluator.
 class ToNative a where
+  -- | Think of 'toNative' as describing how to unify the /result/ of a function with the complete
+  -- list of 'Term's given.  Usually, the list will only have one value in it, but it can have more
+  -- or fewer in the case of e.g., tuples.  Implementations /must/ ground-out every variable in the
+  -- list, or the mode-checker will become unsound.
   toNative :: MonadIO m => a -> [Term EVar] -> AvaleryarT m ()
+
+  -- | Probably this should be 'outMode' for each argument expected in the list of 'Term's in
+  -- 'toNative'.
   inferMode :: [Mode TextVar]
 
 instance ToNative Value where
@@ -198,6 +288,9 @@ instance ToNative a => ToNative (Maybe a) where
   toNative ma xs = toNative (toList ma) xs
   inferMode      = inferMode @[a]
 
+-- | Pretty much just a 1-tuple, like @Only@ from @postgresql-simple@.
+newtype Solely a = Solely a
+
 instance Valuable a => ToNative (Solely a) where
   toNative (Solely a) args = unifyArgs [val a] args
   inferMode = [outMode]
@@ -222,15 +315,23 @@ instance (Valuable a, Valuable b, Valuable c, Valuable d, Valuable e, Valuable f
   toNative (a, b, c, d, e, f) args = unifyArgs [val a, val b, val c, val d, val e, val f] args
   inferMode = [outMode, outMode, outMode, outMode, outMode, outMode]
 
+-- | This is where the magic happens.  We require 'Valuable' (rather than 'ToNative') of the input
+-- so we can use 'fromValue' to pull the value back from Soutei into Haskell.  We assign 'inMode'
+-- here to ensure that we actually get a value from the substitution so that 'fromValue' might
+-- conceivably work.
 instance (Valuable a, ToNative b) => ToNative (a -> b) where
   toNative f (x:xs) = do
-    Val x' <- subst x
+    Val x' <- subst x -- mode checking should make this safe (because of the 'inMode' below)
     case fromValue x' of
       Just a  -> toNative (f a) xs
       Nothing -> empty
   toNative _ _      = empty
   inferMode = inMode : inferMode @b
 
+-- | Executes the IO action and produces the result.
+--
+-- TODO: This should possibly cache the result, but only once per query, probably.  That would
+-- likely require infrastructure we lack at present.
 instance ToNative a => ToNative (IO a) where
   toNative ma xs = do
     a <- liftIO ma
@@ -238,6 +339,7 @@ instance ToNative a => ToNative (IO a) where
 
   inferMode = inferMode @a
 
+-- | Create a native predicate from a 'ToNative' instance with the given name.
 mkNativePred :: forall a m. (ToNative a, MonadIO m) => Text -> a -> NativePred m
 mkNativePred pn f = NativePred np moded
   where np (Lit _ args) = toNative f args
@@ -250,5 +352,7 @@ mkNativeFact a = NativePred np $ fmap Out f
   where f@(Lit _ args)   = vacuous $ toFact a
         np (Lit _ args') = unifyArgs args args'
 
+-- | Create a native database with the given assertion name from the given list of native
+-- predicates.
 mkNativeDb :: Monad m => Text -> [NativePred m] -> NativeDb m
 mkNativeDb assn preds = NativeDb . Map.singleton assn $ Map.fromList [(p, np) | np@(NativePred _ (Lit p _)) <- preds]
